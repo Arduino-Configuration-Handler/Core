17b2d99f56c92f1da17c8510ce81e0bec174c51d
SagiZiv3 <73391391+SagiZiv3@users.noreply.github.com>
2024-10-27 01:45:03 +0200

    Updated the library entry header
    The header now imports the core header files.

--- src/config-handler-core.h
+++ src/config-handler-core.h
@@ -1,4 +1,8 @@
 #ifndef __H_CONFIG_HANDLER_CORE__
 #define __H_CONFIG_HANDLER_CORE__
 
-#endif  // __H_CONFIG_HANDLER_CORE__
+#include "ConfigurationHandler.h"
+#include "ConfigurationUtils.h"
+#include "DataStructures.h"
+
+#endif // __H_CONFIG_HANDLER_CORE__
